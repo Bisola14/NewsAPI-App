1753a220896b3940403a5411f285bd2241890d1c
Bisola14 <167443356+Bisola14@users.noreply.github.com>
2024-04-18 19:12:38 +0300

    Removed API key from Python script

--- news_fetcher.py
+++ news_fetcher.py
@@ -1,18 +1,17 @@
 import requests
+from config import API_KEY
 
-def fetch_top_news(api_key):
+def fetch_top_news():
     url = "https://newsapi.org/v2/top-headlines"
     params = {
-        "apiKey": api_key,
+        "apiKey": API_KEY,
         "country": "us"  # You can change the country code as per your preference
     }
     response = requests.get(url, params=params)
     data = response.json()
     return data["articles"]
 
-# Replace 'your_api_key' with your actual News API key
-api_key = '238f84865d5f4a4d83ae456c009d2961'
-top_news = fetch_top_news(api_key)
+top_news = fetch_top_news()
 for news in top_news:
     print(news["title"])
     print(news["description"])
